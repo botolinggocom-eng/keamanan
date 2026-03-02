e37906b035b8be24ea9983d10bc1b3122a474dc3
botolinggocom-eng <botolinggocom@gmail.com>
2026-03-02 15:42:21 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,11 @@ importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAo-nP5XZGK8NqMsjSiHb64--qqUZgB_eM",
-  authDomain: "siagadesa10.firebaseapp.com",
-  databaseURL: "https://siagadesa10-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "siagadesa10",
-  storageBucket: "siagadesa10.appspot.com",
-  messagingSenderId: "24091093796",
-  appId: "1:24091093796:web:6773efecd9f7a6f4e0a1b1"
+  apiKey:"AIzaSyAo-nP5XZGK8NqMsjSiHb64--qqUZgB_eM",
+  authDomain:"siagadesa10.firebaseapp.com",
+  projectId:"siagadesa10",
+  messagingSenderId:"24091093796",
+  appId:"1:24091093796:web:6773efecd9f7a6f4e0a1b1"
 });
 
-const messaging = firebase.messaging();
+firebase.messaging();
